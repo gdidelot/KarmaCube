9d06ccd6c6f6d03b2986c4baeb9588e7737ed04c
gdidelot <gdidelot@live.fr>
2019-07-12 18:15:17 +0200

    Implémentation de services

--- sources/serveur/components/gestionquetes/quetes.services.php
+++ sources/serveur/components/gestionquetes/quetes.services.php
@@ -70,34 +70,157 @@ class Quetes implements IQuetes
 	
 	public function obtenirQuetesUtilisateur($utilisateur)
 	{
+		Serveur\Communs\Logger::Info("Quetes.obtenirQuetes : Retourner quetes utilisateur");
+		
+		$response = null;
+		
+		try
+		{	
+			$quetes = $this->queteRepository->findBy(array('Utilisateur' => $utilisateur));
+
+			$response = new Serveur\Communs\ServiceResponse($quetes);
+			
+			Serveur\Communs\Logger::Info("Quetes.obtenirQuetes : Obtenir les quetes terminée");
+		}
+		catch (\Exception $ex) 
+		{
+			$response = Serveur\Communs\ServiceResponse::CreateError($ex);
+		}
+		
+		return $response;
 	}
 	
 	public function ajouterQuete($quete)
-	{
+	
+	{Serveur\Communs\Logger::Info("Quetes.obtenirQuetes : Retourner ajouter quete");
+		
+		$response = null;
+		
+		try
+		{	
+			$quetes = $this->queteRepository->findBy(array('Utilisateur' => $utilisateur));
+
+			$response = new Serveur\Communs\ServiceResponse($quetes);
+			
+			Serveur\Communs\Logger::Info("Quetes.obtenirQuetes : Obtenir les quetes terminée");
+		}
+		catch (\Exception $ex) 
+		{
+			$response = Serveur\Communs\ServiceResponse::CreateError($ex)
 	}
 	
 	public function modifierQuete($quete)
-	{
+
+	{Serveur\Communs\Logger::Info("Quetes.obtenirQuetes : Retourner modifier quete");
+		
+		$response = null;
+		
+		try
+		{	
+			$quetes = $this->queteRepository->findBy(array('Utilisateur' => $utilisateur));
+
+			$response = new Serveur\Communs\ServiceResponse($quetes);
+			
+			Serveur\Communs\Logger::Info("Quetes.obtenirQuetes : Obtenir les quetes terminée");
+		}
+		catch (\Exception $ex) 
+		{
+			$response = Serveur\Communs\ServiceResponse::CreateError($ex)
 	}
 	
 	public function supprimerQuete($quete)
-	{
+
+	{Serveur\Communs\Logger::Info("Quetes.obtenirQuetes : Retourner supprimer quete");
+		
+		$response = null;
+		
+		try
+		{	
+			$quetes = $this->queteRepository->findBy(array('Utilisateur' => $utilisateur));
+
+			$response = new Serveur\Communs\ServiceResponse($quetes);
+			
+			Serveur\Communs\Logger::Info("Quetes.obtenirQuetes : Obtenir les quetes terminée");
+		}
+		catch (\Exception $ex) 
+		{
+			$response = Serveur\Communs\ServiceResponse::CreateError($ex)
 	}
 	
 	public function obtenirEtapes($quete)
-	{
+	
+	{Serveur\Communs\Logger::Info("Quetes.obtenirEtapes : Retourner obtenir etapes");
+		
+		$response = null;
+		
+		try
+		{	
+			$etapeQuetes = $this->etapeQueteRepository->findBy(array('Quete' => $quete));
+
+			$response = new Serveur\Communs\ServiceResponse($etapeQuetes);
+			
+			Serveur\Communs\Logger::Info("Quetes.obtenirEtapes : Obtenir les etapes terminée");
+		}
+		catch (\Exception $ex) 
+		{
+			$response = Serveur\Communs\ServiceResponse::CreateError($ex)
 	}
 	
 	public function ajouterEtape($etapeQuete)
-	{
+
+	{Serveur\Communs\Logger::Info("Quetes.obtenirEtapes : Retourner ajouter etapes");
+		
+		$response = null;
+		
+		try
+		{	
+			$etapeQuetes = $this->etapeQueteRepository->findBy(array('Quete' => $quete));
+
+			$response = new Serveur\Communs\ServiceResponse($etapeQuetes);
+			
+			Serveur\Communs\Logger::Info("Quetes.obtenirEtapes : Obtenir les etapes terminée");
+		}
+		catch (\Exception $ex) 
+		{
+			$response = Serveur\Communs\ServiceResponse::CreateError($ex)
 	}
 	
 	public function modifierEtape($etapeQuete)
-	{
+
+	{Serveur\Communs\Logger::Info("Quetes.obtenirEtapes : Retourner modifier etapes");
+		
+		$response = null;
+		
+		try
+		{	
+			$etapeQuetes = $this->etapeQueteRepository->findBy(array('Quete' => $quete));
+
+			$response = new Serveur\Communs\ServiceResponse($etapeQuetes);
+			
+			Serveur\Communs\Logger::Info("Quetes.obtenirEtapes : Obtenir les etapes terminée");
+		}
+		catch (\Exception $ex) 
+		{
+			$response = Serveur\Communs\ServiceResponse::CreateError($ex)
 	}
 	
 	public function supprimerEtape($etapeQuete)
 	{
+		Serveur\Communs\Logger::Info("Quetes.obtenirEtapes : Retourner supprimer etapes");
+		
+		$response = null;
+		
+		try
+		{	
+			$etapeQuetes = $this->etapeQueteRepository->findBy(array('Quete' => $quete));
+
+			$response = new Serveur\Communs\ServiceResponse($etapeQuetes);
+			
+			Serveur\Communs\Logger::Info("Quetes.obtenirEtapes : Obtenir les etapes terminée");
+		}
+		catch (\Exception $ex) 
+		{
+			$response = Serveur\Communs\ServiceResponse::CreateError($ex)
 	}
 	
 }
