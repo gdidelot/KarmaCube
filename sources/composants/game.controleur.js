f1f0ffc42371fd7ae4ae91223e232cfb99f2b0c0
gdidelot <gdidelot@live.fr>
2020-02-12 21:03:26 +0100

    controls

--- sources/composants/game.controleur.js
+++ sources/composants/game.controleur.js
@@ -21,90 +21,76 @@ app.controller('gamecontroleur', ["$injector", "$scope", "$location", function($
 	var scene = undefined;
 	var camera = undefined;
 	var renderer = undefined;
-	var cube = undefined;
+	var meshes = [];
 	var controls = undefined;
 	var container = undefined;
-	var stats = undefined;
-	var camera= undefined;
-	var worldWidth = 200;
-	var worldDepth = 200;
-	var worldHalfWidth = worldWidth / 2;
-	var worldHalfDepth = worldDepth / 2;
-	var data = undefined;
-	var clock = new THREE.Clock();
 	
     //#endregion Variables declarations
 	
 	//#region Private functions
 
-	var onWindowResize = function() {
-		camera.aspect = window.innerWidth / window.innerHeight;
-		camera.updateProjectionMatrix();
-		renderer.setSize( window.innerWidth, window.innerHeight );
-
-	};
-
-	var getY = function( x, z ) {
-		return ( data[ x + z * worldWidth ] * 0.2 ) | 0;
-	};
-			
-	var generateHeight = function( width, height ) {
-		var data = [], perlin = new THREE.ImprovedNoise(), size = width * height, quality = 2, z = Math.random() * 100;
-		for ( var j = 0; j < 4; j ++ ) {
-			if ( j == 0 ) for ( var i = 0; i < size; i ++ ) data[ i ] = 0;
-			for ( var i = 0; i < size; i ++ ) {
-				var x = i % width, y = ( i / width ) | 0;
-				data[ i ] += perlin.noise( x / quality, y / quality, z ) * quality;
-			}
-			quality *= 4;
-		}
-		return data;
-	};
-	
 	var animate = function() {
 		requestAnimationFrame( animate );
-				
-		mesh1.rotation.x += 0.000;
-		mesh1.rotation.y += 0.000;
-
-		renderer.render( scene, camera );
-	};
-
-	var render = function() {
-		controls.update( clock.getDelta() );
+		
+		angular.forEach(meshes, function(mesh){
+			mesh.rotation.x += 0.000;
+			mesh.rotation.y += 0.000;
+		});
+		
+		controls.update();
+		
 		renderer.render( scene, camera );
 	};
 	
+	var genererCube = function(texture, positionX, positionY) {
+		var texture = new THREE.TextureLoader().load( 'themes/defaut/images/textures/' + texture);
+		var geometry = new THREE.BoxBufferGeometry( 200, 200, 200 );
+		var material = new THREE.MeshBasicMaterial( { map: texture } );
+		var mesh = new THREE.Mesh( geometry, material );
+		mesh.position.x = positionX;
+		mesh.position.y = positionY === undefined ? 0 : positionY;
+		mesh.position.z = 0;
+		mesh.addEventListener( 'click', onMeshClick, false );
+		meshes.push(mesh);
+		scene.add( mesh );
+		return mesh;
+	};
+
     var initialisationScene = function() {
-		camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
-		camera.position.z = 400;
-
+		camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
+		camera.position.x =   0;
+		camera.position.y = 1.5;
+		camera.position.z = 1000;
+		
 		scene = new THREE.Scene();
-
-		var texture1 = new THREE.TextureLoader().load( 'themes/defaut/images/textures/ciel.jpg');
-		var texture2 = new THREE.TextureLoader().load( 'themes/defaut/images/textures/bibliotheque.jpg');
-
-		var geometry1 = new THREE.BoxBufferGeometry( 200, 200, 200 );
-		var geometry2 = new THREE.BoxBufferGeometry( 200, 200, 200 );
-		var material1 = new THREE.MeshBasicMaterial( { map: texture1 } );
-		var material2 = new THREE.MeshBasicMaterial( { map: texture2 } );
-
-		mesh1 = new THREE.Mesh( geometry1, material1 );
-		mesh2 = new THREE.Mesh( geometry2, material2 );
 		
-		mesh2.position.x = 210;
-		mesh2.position.y = 0;
-		mesh2.position.z = 0;
+		// Génération de la grille
+		var size = 10, step = 1;
+		var geometry = new THREE.Geometry();
+		var material = new THREE.LineBasicMaterial({color: 'white'});
+		for(var i = -size;i <= size; i += step) {
+			geometry.vertices.push(new THREE.Vector3(-size, -0.04, i));
+			geometry.vertices.push(new THREE.Vector3( size, -0.04, i));
+			
+			geometry.vertices.push(new THREE.Vector3( i, -0.04, -size));
+			geometry.vertices.push(new THREE.Vector3( i, -0.04,  size));
+		}
+		var line = new THREE.Line(geometry, material, THREE.LineSegments);
+		scene.add(line);
 		
-		scene.add( mesh1 );
-		scene.add( mesh2 );
+		genererCube('ciel.jpg', meshes.length * 200);
+		genererCube('ciel.jpg', meshes.length * 200);
 
 		renderer = new THREE.WebGLRenderer( { antialias: true } );
 		renderer.setPixelRatio( window.devicePixelRatio );
 		renderer.setSize( window.innerWidth, window.innerHeight );
 		document.body.appendChild( renderer.domElement );
+		
+		controls = new THREE.OrbitControls( camera, renderer.domElement );
+		controls.update();
 
 		window.addEventListener( 'resize', onWindowResize, false );
+		window.addEventListener( 'click', onWindowClick, false );
 	};
 	
 	/**
@@ -132,6 +118,31 @@ app.controller('gamecontroleur', ["$injector", "$scope", "$location", function($
 	//#endregion Public functions
 	
 	//#region Events management
+	
+	var onMeshClick = function(event) {
+		alert(event);
+	};
+	
+	var onWindowClick = function(event) {
+		 event = event || window.event;
+		var pageX = event.pageX;
+		var pageY = event.pageY;
+
+		// IE 8
+		if (pageX === undefined) {
+			pageX = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+			pageY = event.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+		}
+
+		console.log(pageX, pageY);
+		genererCube('ciel.jpg', pageX, -pageY);
+	};
+	
+	var onWindowResize = function() {
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+		renderer.setSize( window.innerWidth, window.innerHeight );
+	};
 
     var onViewContentLoaded = $scope.$watch('$viewContentLoaded', function () {
 		console.log('gamecontroleur received $viewContentLoaded');
@@ -142,6 +153,7 @@ app.controller('gamecontroleur', ["$injector", "$scope", "$location", function($
         console.log('gamecontroleur destroy.');
         destroyOn();
         onViewContentLoaded();
+		onWindowResize();
     });
 
     //#endregion Events management
